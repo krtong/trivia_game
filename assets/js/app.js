c51d874150b80fcf411b134538c62e23423e5d8b
krtong <kevin.r.tong@gmail.com>
2019-10-19 18:11:59 -0700

    correct/incorrect trivia answer displays correct information

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,63 +1,76 @@
 class TriviaQuestion {
-    constructor(question, correct, incorrectArr, img ){
-        Object.assign(this, {question, correct, incorrectArr, img})
+    constructor(question, correct, incorrectArr, img) {
+        Object.assign(this, {
+            question,
+            correct,
+            incorrectArr,
+            img
+        })
     }
 }
 
 let questions = {
-     questionOne : new TriviaQuestion (
+    questionOne: new TriviaQuestion(
         `Who wrote "crazy in love?"`,
         `4`,
         ["1", "2", "3"],
-        "./assets/img/beyonce.webp"
+        "beyonce.webp"
     ),
-    questionTwo : new TriviaQuestion (
+    questionTwo: new TriviaQuestion(
         `Who wrote "Tub Thumper?"`,
         `9`,
         ["6", "7", "8"],
-        "./assets/img/beyonce.webp"
+        "beyonce.webp"
     ),
-    questionThree : new TriviaQuestion (
+    questionThree: new TriviaQuestion(
         `Who wrote "I like big butts and i cannot lie?"`,
         `13`,
         ["10", "11", "12"],
-        "./assets/img/beyonce.webp"
+        "beyonce.webp"
     ),
-    
+
 };
 
-
 //functions
-function random (value) { 
+function random(value) {
     return Math.floor(Math.random() * value)
 };
 
-function initialize () {
-    let objArr = Object.values(questions);
+function initialize() {
+    let objArr = Object.values(questions); //randomizes the question
     let randomIdx = random(objArr.length);
     let [question, correct, incorrectArr, img] = Object.values(objArr[randomIdx]);
 
     incorrectArr.push(correct);
-
-    for (let i = 0; i < 3; i++) {
-        if (random(100) < 50)  {
-            let temp = incorrectArr[i+1]
-            incorrectArr[i+1] = incorrectArr[i];
+    
+    for (let i = 0; i < 3; i++) { //randomizes the order of  multiple choice answers.
+        if (random(100) < 50) {
+            let temp = incorrectArr[i + 1]
+            incorrectArr[i + 1] = incorrectArr[i];
             incorrectArr[i] = temp;
         }
-    }
-    
+    };
 
     let html = `<ul>`;
-    incorrectArr.map(function(choice) {
-        html += `<li>${choice}</li>`
-    });
+    incorrectArr.map(choice => html += `<li class="choice">${choice}</li>`);
+
+    function isCorrect() {
+        let message = $(this).html() === correct ? "Correct!" : `Incorrect! The answer was ${correct}.`;
+        let html = `<div>
+                        <div><h1>${message}</h1></div>
+                        <img src="./assets/img/${img}" alt="">
+                    </div> `;
+
+        $("#multiple-choice").empty()
+        $("#question").html(html);
+    };
 
     $("#question").html(question);
     $("#multiple-choice").html(html + "</ul>")
+    $(".choice").click(isCorrect)
 };
 
 
 
 //click handlers
-    $("#start").click(initialize);
+$("#start").click(initialize);
