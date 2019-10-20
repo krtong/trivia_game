64d0d79036281129b3b32c3ccc58fa71120b8cd0
krtong <kevin.r.tong@gmail.com>
2019-10-19 22:22:42 -0700

    logic working. needs end-game screen, refactoring and styling.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,65 +12,73 @@ class TriviaQuestion {
 let questions = {
     questionOne: new TriviaQuestion(
         `Who wrote "crazy in love?"`,
-        `4`,
-        ["1", "2", "3"],
+        `Beyonce`,
+        ["Beyonce", "Jay-Z", "Desinty's Child", "Rhianna"],
         "beyonce.webp"
     ),
     questionTwo: new TriviaQuestion(
         `Who wrote "Tub Thumper?"`,
-        `9`,
-        ["6", "7", "8"],
+        `Chumbawumba`,
+        ["Chumbawumba", "Red Hot Chili Peppers", "Blackfish", "Beck"],
         "beyonce.webp"
     ),
     questionThree: new TriviaQuestion(
         `Who wrote "I like big butts and i cannot lie?"`,
-        `13`,
-        ["10", "11", "12"],
+        `Sir Mix a lot`,
+        [`Sir Mix a lot`, "Redman", "Ghostface Killah", "Africa Bambataa"],
         "beyonce.webp"
     ),
-
 };
 
 //functions
-function random(value) {
-    return Math.floor(Math.random() * value)
-};
-
 function initialize() {
+    let timesUp = false;
     let objArr = Object.values(questions); //randomizes the question
-    let randomIdx = random(objArr.length);
-    let [question, correct, incorrectArr, img] = Object.values(objArr[randomIdx]);
-
-    incorrectArr.push(correct);
-    
-    for (let i = 0; i < 3; i++) { //randomizes the order of  multiple choice answers.
-        if (random(100) < 50) {
-            let temp = incorrectArr[i + 1]
-            incorrectArr[i + 1] = incorrectArr[i];
-            incorrectArr[i] = temp;
-        }
-    };
-
+    let randomIdx = Math.floor(Math.random() * objArr.length);
+    let [question, correct, arr, img] = Object.values(objArr[randomIdx]);
+    let intervalId;
     let html = `<ul>`;
-    incorrectArr.map(choice => html += `<li class="choice">${choice}</li>`);
+
+    //this section is for populating the page with multiple choice answers. 
+    arr = arr.sort((a, b) => Math.random() > 0.5 ? 1 : -1);
+    arr.map(choice => html += `<li class="choice">${choice}</li>`);
 
     function isCorrect() {
-        let message = $(this).html() === correct ? "Correct!" : `Incorrect! The answer was ${correct}.`;
+        let message;
+        if (timesUp) message = [`Time's up.`, `The answer was ${correct}.`]
+        else if ($(this).html() === correct) message = [`Correct!`, ``]
+        else message = [`Incorrect!`, `The answer was ${correct}.`];
         let html = `<div>
-                        <div><h1>${message}</h1></div>
-                        <img src="./assets/img/${img}" alt="">
+                        <div><h1>${message[0]}</h1></div>
+                        <div>${message[1]}</div>
+                        <img src="./assets/img/${img}">
                     </div> `;
-
         $("#multiple-choice").empty()
+        $("#timer").empty();
         $("#question").html(html);
+        clearInterval(intervalId)
+        setTimeout(initialize, 5000);
     };
 
+    //this section is for creating the timer. 
+    function run(number) {
+        clearInterval(intervalId);
+        $("#timer").html("<h2>" + number + "</h2>");
+        intervalId = setInterval(function () {
+            number--;
+            $("#timer").html(`<h5>${number}</h5>`);
+            if (number === 0) {
+                timesUp = true;
+                clearInterval(intervalId);
+                isCorrect();
+            }
+        }, 1000);
+    };
+    
+    run(10);
     $("#question").html(question);
-    $("#multiple-choice").html(html + "</ul>")
+    $("#multiple-choice").html(`${html}</ul>`)
     $(".choice").click(isCorrect)
 };
 
-
-
-//click handlers
 $("#start").click(initialize);
